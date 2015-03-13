587ad77930cfaa950dc0dad5b58d8b3797cb8ca1
wolfgangleon <wolfgangrleonh@gmail.com>
2015-03-13 10:58:54 -0400

    trail API working

--- script/app.js
+++ script/app.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
 
+	//Hover over nav elements
 	$('#interactive .navbar li').hover(function() {
 		$(this).toggleClass('active');
 	});
 
+	//Click on CrossRoad title to reload the page
+	$('#interactive .navbar-brand').on('click', function() {
+		location.reload();
+	})
+
+	//Show nav bar elements info
+	$('nav #link-what').on('click', function(){
+		$('#what').fadeIn();
+	})
+
+
+	//Back to top button
 	$(window).scroll(function(){
 	   if( $(document).scrollTop() > 0 ) {
 	      $('#backToTop').show();
@@ -12,10 +25,7 @@ $(document).ready(function(){
 	   }
 	});
 
-	$('#interactive .navbar-brand').on('click', function() {
-		location.reload();
-	})
-
+	
 	//Button Selects an activtity
 	$('#select-activities .btn').on('click', function(){
 
@@ -30,12 +40,13 @@ $(document).ready(function(){
 	//Submit button Search
 	$('#get-location').submit(function() {
 
+		$('#search-result').html('');
+
 		$('#results').fadeIn();
 		
 		$('html,body').animate({scrollTop: $('#results').offset().top},800 );
 
 		var activity = $('#selected-activity').html(),
-
 			location = $('#location').val();
 
 			console.log('selected-activity:' + activity)
@@ -43,8 +54,7 @@ $(document).ready(function(){
 
 		var params = {	
 			api_key:'3fb0455c6af396a7fcfff852600cb619',
-			limit:10,
-			//activities_activity_type_name_eq: activity,
+			limit:25,
 			q:{
 				activities_activity_type_name_cont:activity, 
 				city_cont:location
@@ -60,6 +70,31 @@ $(document).ready(function(){
 
 				$.each(result.places, function(i,place) {
 					console.log('place:', place)
+					console.log('activities:' , place.activities[0].description )
+
+					var item = $('.template #location-info').clone();
+
+					//Set item title 
+					var nameItem = item.find('#location-name');
+						nameItem.text( place.name )
+
+					// Set item description 
+					var descripItem = item.find('#location-description');
+						descripItem.text( place.activities[0].description)
+						$('#location-description').find('script').remove();
+
+					// Set item image 
+					var thumbnail = item.find('#location-image');
+
+						if ( place.activities[0].thumbnail == null ) {
+							thumbnail.attr('src', 'http://www.bluehillsfarmshop.co.uk/images/blank.gif')
+						} 
+						else {
+							thumbnail.attr('src', place.activities[0].thumbnail)
+						};
+
+					$('#search-result').append(item)
+
 				});
 
 			});
